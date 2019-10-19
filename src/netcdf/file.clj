baea8a7231be9e6724783615fa8d7e77a4d0c688
Tim Cross <theophilusx@gmail.com>
2019-10-19 15:39:01 +1100

    Completed basic file IO functions and tests

--- src/netcdf/file.clj
+++ src/netcdf/file.clj
@@ -1,8 +1,7 @@
 (ns netcdf.file
   (:import [ucar.nc2 NetcdfFile]
            [java.io FileNotFoundException IOException])
-  ;(:require [clj-netcdf.groups :as groups])
-  )
+  (:require [clojure.string :as string]))
 
 (defn open
   "Open a NetCDF file. Return a `NetcdfFile` object. `location` is a path
@@ -42,4 +41,120 @@
   (.close nc)
   true)
 
+(defn can-open?
+  "Test to see if we can open a NeetCDF file without actually opening
+  it. `location` is a path to a `NetcdfFile` object."
+  [location]
+  (let [file (NetcdfFile/canonicalizeUriString location)]
+    (try
+      (NetcdfFile/canOpen file)
+      (catch Exception e
+        false))))
 
+(defn type-id
+  "Get the file type id for the underlying data source. The `nc` argument is a
+  `NetcdfFile` object returned from a call to `open` or `open-in-memory`"
+  [nc]
+  {:pre [(= (type nc) NetcdfFile)]}
+  (.getFileTypeId nc))
+
+(defn location
+  "Return the location of the NetCDF file. Argument `nc` is a `NetcdfFile` object
+  returned from a call to `open` or `open-in-memory`."
+  [nc]
+  {:pre [(= (type nc) NetcdfFile)]}
+  (.getLocation nc))
+
+(defn -mk-var-info-map [line]
+  (let [[var-name start size unlim] (string/split line #" +")]
+    {(keyword var-name) {:start start
+                         :size size
+                         :unlimited (if (= unlim "true")
+                                      true
+                                      false)}}))
+
+(defn -mk-cache-info-map [line]
+  (let [[var-name cached size c-size] (string/split line #" +")]
+    {(keyword var-name) {:cached? (if (= cached "true")
+                                    true
+                                    false)
+                         :size (or (clojure.edn/read-string size)
+                                   0)
+                         :cached-size (or (clojure.edn/read-string c-size)
+                                          0)}}))
+
+(defn -parse-iosp-tables [data]
+  (let [state (reduce (fn [acc line]
+               (cond
+                 (or (string/index-of line "=")
+                     (empty? line)) acc
+                 (string/starts-with? line "name____start")
+                 (assoc acc :state :var-info)
+                 (string/starts-with? line "Variable  isCaching")
+                 (assoc acc :state :cache-info)
+                 :else
+                 (if (= (:state acc) :var-info)
+                   (let [var-data (-mk-var-info-map line)]
+                     (assoc acc :var-info (merge (:var-info acc) var-data)))
+                   (if (= (:state acc) :cache-info)
+                     (let [cache-data (-mk-cache-info-map line)]
+                       (assoc acc :cache-info
+                              (merge (:cache-info acc) cache-data)))))))
+                      {:state :unknown :var-info {} :cache-info {}} data)]
+    {:var-info (:var-info state)
+     :cache-info (:cache-info state)}) )
+
+(defn -mk-iosp-var-map [data]
+  (reduce (fn [acc v]
+            (if (= (count v) 2)
+              (assoc acc (first v) (second v))
+              acc))
+          {} (map (fn [line]
+                    (if (and (string/index-of line "=")
+                             (not (string/starts-with? line "total")))
+                      (let [[name val] (string/split line #"=")]
+                        [(keyword (string/replace (string/trim name) " " "-"))
+                         (string/trim val)])))
+                  data)))
+
+(defn iosp-info
+  "Return details of underlying ISOP information. `nc` is a `NetcdfFile` object
+  returns from a call to `open` or `open-in-memory`. Returned value is a map."
+  [nc]
+  {:pre [(= (type nc) NetcdfFile)]}
+  (let [raw (map string/trim (string/split-lines (.getDetailInfo nc)))
+        var-list-map (-mk-iosp-var-map raw)
+        table-map (-parse-iosp-tables raw)]
+    (merge var-list-map table-map)))
+
+(defn file-type-description
+  "Return description of file type. `nc` is a `NetcdfFile` object returned from a
+  call to `open` or `open-in-memory`. Returns a `string`"
+  [nc]
+  {:pre [(= (type nc) NetcdfFile)]}
+  (.getFileTypeDescription nc))
+
+(defn file-type-version
+  "Get the file type version for the file. `nc` is a `NetcdfFile` object returned
+  from a call to `open` or `open-in-memory`. Returns a `string`."
+  [nc]
+  {:pre [(= (type nc) NetcdfFile)]}
+  (.getFileTypeVersion nc))
+
+(defmacro with-netcdf
+  "A macro for opening and processing a NetCDF file, performing some operations, 
+  and then closing the file. Arguments are a `name` to refer to the opened NetCDF
+  object, a `file` to open and a `body` of expressions to execute."
+  [[name file] & body]
+  `(let [~name (open ~file)]
+     ~@body
+     (close ~name)))
+
+(defmacro with-memory-netcdf
+  "Same as `with-netcdf`, but loads the file into memory. Arguments are a 
+  `name` to refer tot he NetCDF object, a `file` specifying the file to operate 
+  on and a `body` of expressions to execute."
+  [[name file] & body]
+  `(let [~name (open-in-memory ~file)]
+    ~@body
+    (close ~name)))
--- test/netcdf/file_test.clj
+++ test/netcdf/file_test.clj
@@ -22,3 +22,59 @@
     (is (thrown? Exception (sut/open-in-memory "no-such-file"))))
   (testing "close invalid nc object throws exception"
     (is (thrown? AssertionError (sut/close nil)))))
+
+(deftest can-open-file
+  (testing "Can open valid file returns true"
+    (is (= (sut/can-open? "test-data/test.nc") true)))
+  (testing "Can open returns false for non-existent file"
+    (is (= (sut/can-open? "no-such-file.nc") false))))
+
+(deftest get-type-id
+  (testing "Can get file type id from existing file"
+    (let [nc (sut/open "test-data/test.nc")]
+      (is (= (sut/type-id nc) "NetCDF"))
+      (sut/close nc)))
+  (testing "type-id throws exception for bad nc"
+    (is (thrown? AssertionError (sut/type-id nil)))))
+
+(deftest get-location
+  (testing "can get location of nc object"
+    (let [nc (sut/open "test-data/test.nc")]
+      (is (= (sut/location nc) "test-data/test.nc"))
+      (sut/close nc)))
+  (testing "location throws assertion error for bad nc"
+    (is (thrown? AssertionError (sut/location nil)))))
+
+(deftest get-iosp-info
+  (testing "return map of IOSP info for valid nc"
+    (let [nc (sut/open "test-data/test.nc")]
+      (is (map? (sut/iosp-info nc)))
+      (sut/close nc)))
+  (testing "throws assertion error for invalid nc"
+    (is (thrown? AssertionError (sut/iosp-info nil)))))
+
+(deftest get-file-type-description
+  (testing "returns string for valid nc"
+    (let [nc (sut/open "test-data/test.nc")]
+      (is (= (sut/file-type-description nc) "NetCDF-3/CDM"))
+      (sut/close nc)))
+  (testing "thows assertion error for invalid nc"
+    (is (thrown? AssertionError (sut/file-type-description nil)))))
+
+(deftest get-file-type-version
+  (testing "returns string representing file type version"
+    (let [nc (sut/open "test-data/test.nc")]
+      (is (= (sut/file-type-version nc) "N/A"))
+      (sut/close nc)))
+  (testing "thorws assertion error for bad nc"
+    (is (thrown? AssertionError (sut/file-type-description nil)))))
+
+(deftest with-netcdf-test
+  (testing "with-netcdf file reads file type details"
+    (sut/with-netcdf [fobj "test-data/test.nc"]
+      (is (= (sut/file-type-description fobj) "NetCDF-3/CDM")))))
+
+(deftest with-memory-netcdf-test
+  (testing "with-memory-netcdf executes"
+    (sut/with-memory-netcdf [f "test-data/test.nc"]
+      (is (= (sut/file-type-description f) "NetCDF-3/CDM")))))

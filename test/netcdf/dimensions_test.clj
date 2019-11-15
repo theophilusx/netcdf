5ad6fe1f8b86e9170066b5d4b88d0f8e1af6685c
Tim Cross <theophilusx@gmail.com>
2019-11-16 07:06:48 +1100

    Updated dimension tests to handle multiple test files

--- test/netcdf/dimensions_test.clj
+++ test/netcdf/dimensions_test.clj
@@ -6,38 +6,46 @@
 
 (def conf (load-config :file "/home/tim/Projects/clojure/netcdf/config.edn"))
 
-(def test-file (str (:test-data conf) "/test.nc"))
+(def test-files ["test.nc" "test_hgroups.nc" "testrh.nc"
+                 "test_echam_spectral-deflated.nc" "OMI-Aura_L2-example.nc"
+                 "access-s.nc"])
+
+(def dimension-keys [:name :length :unlimited? :varying? :obj])
+
+(def test-file (atom ""))
 
 (deftest dimensions-test
-  (with-netcdf [nc test-file]
-    (let [dim (sut/dimensions nc)]
-      (testing "returns a vector"
-        (is (vector? dim)))
-      (testing "returns correct dimension count"
-        (is (= (count dim) 3)))
-      (testing "returned vector of maps with correct keys"
-        (is (every? (fn [d]
-                      (= [:name :length :unlimited? :varying? :obj] (keys d)))
-                    dim))))))
+  (testing (str "Testing with file " @test-file)
+    (with-netcdf [nc @test-file]
+      (let [dim (sut/dimensions nc)]
+        (testing "returns a vector"
+          (is (vector? dim)))
+        (testing "returns correct dimension count"
+          (is (> (count dim) 0)))
+        (testing "returned vector of maps with correct keys"
+          (is (every? (fn [d]
+                        (=  dimension-keys (keys d)))
+                      dim)))))))
 
 (deftest dimension-test
-  (testing "latitude dimension has correct keys and values"
-    (with-netcdf [nc test-file]
-      (let [dim (sut/dimension nc "latitude")]
-        (is (= [:name :length :unlimited? :varying? :obj] (keys dim)))
-        (is (= (:name dim) "latitude"))
-        (is (= (:length dim) 735))
-        (is (= (:unlimited? dim) false))
-        (is (= (:varying? dim) false)))))
-  (testing "time dimension has correct keys and values"
-    (with-netcdf [nc test-file]
-      (let [dim (sut/dimension nc "time")]
-        (is (= [:name :length :unlimited? :varying? :obj] (keys dim)))
-        (is (= (:name dim) "time"))
-        (is (= (:length dim) 161))
-        (is (= (:unlimited? dim) false))
-        (is (= (:varying? dim) false)))))
-  (testing "non-existent dimension returns nil"
-    (with-netcdf [nc test-file]
-      (is (nil? (sut/dimension nc "notexist"))))))
+  (testing (str "Testing with file " @test-file)
+    (with-netcdf [nc @test-file]
+      (let [d-names (map #(:name %) (sut/dimensions nc))
+            ]
+        (doseq [d d-names]
+          (when-let [dim (sut/dimension nc d)]
+            (testing (str "dimension " d)
+              (is (= dimension-keys (keys dim)))
+              (is (= (:name dim) d))
+              (is (number? (:length dim)))
+              (is (boolean? (:unlimited? dim)))
+              (is (boolean? (:varying? dim))))))))))
 
+(deftest dimensions-testing
+  (doseq [f test-files]
+    (reset! test-file (str (:test-data conf) "/" f))
+    (dimensions-test)
+    (dimension-test)))
+
+(defn test-ns-hook []
+  (dimensions-testing))

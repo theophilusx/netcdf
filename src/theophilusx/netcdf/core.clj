5279468a1d8aa54d7d22e2417369aca293eb17c2
Tim Cross <theophilusx@gmail.com>
2020-08-14 09:46:01 +1000

    Alias public API functions in core namespace

--- src/theophilusx/netcdf/core.clj
+++ src/theophilusx/netcdf/core.clj
@@ -1,21 +1,46 @@
 (ns theophilusx.netcdf.core
-  (:require [theophilusx.netcdf.file :as f]))
+  (:require [theophilusx.netcdf.file :as files]
+            [theophilusx.netcdf.variables :as vars]
+            [theophilusx.netcdf.attributes :as attrs]
+            [theophilusx.netcdf.dimensions :as dims]
+            [theophilusx.netcdf.groups :as grps]
+            [theophilusx.netcdf.ranges :as rngs]))
+
+(def open files/open)
+(def open-in-memory files/open-in-memory)
+(def close files/close)
 
 (defmacro with-netcdf
   "A macro for opening and processing a NetCDF file, performing some operations,
   NetCDF object, a `file` to open and a `body` of expressions to execute."
   [[name file] & body]
-  `(let [~name (f/open ~file)]
+  `(let [~name (files/open ~file)]
      ~@body
-     (f/close ~name)))
+     (files/close ~name)))
 
 (defmacro with-memory-netcdf
   "Same as `with-netcdf`, but loads the file into memory. Arguments are a 
   `name` to refer tot he NetCDF object, a `file` specifying the file to operate 
   on and a `body` of expressions to execute."
   [[name file] & body]
-  `(let [~name (f/open-in-memory ~file)]
+  `(let [~name (files/open-in-memory ~file)]
      ~@body
-     (f/close ~name)))
+     (files/close ~name)))
+
+(def variable vars/variable)
+(def variables vars/variables)
+
+(def attribute attrs/attribute)
+(def global-attribute attrs/global-attribute)
+(def global-attributes attrs/global-attributes)
+
+(def dimension dims/dimension)
+(def dimensions dims/dimensions)
+
+(def root-group grps/root-group)
+(def group grps/group)
+
+(def make-range rngs/make-range)
+(def make-named-range rngs/make-named-range)
 
 
